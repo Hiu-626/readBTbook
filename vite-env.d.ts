4e88c62054a6f7b984c9ed15ab66b59e5cf7efab
HIU626 <onlywater96@gmail.com>
2026-02-03 09:22:54 +1100

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,3 @@
-// Removed reference to vite/client as it was causing resolution errors in this environment
-// /// <reference types="vite/client" />
-
 interface ImportMetaEnv {
   readonly VITE_FIREBASE_API_KEY: string;
   readonly VITE_FIREBASE_AUTH_DOMAIN: string;
